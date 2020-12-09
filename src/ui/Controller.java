163e7d85c99e1461c2470032e81f03105a4ec48e
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2020-12-09 12:08:50 +0100

    mac color picker

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -3,9 +3,27 @@ package ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller {
+
+    @FXML
+    private Pane topPane;
+    @FXML
+    private Button buttonX;
+    @FXML
+    private Button buttonMinimize;
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private Pane backgroundPane;
+    @FXML
+    private Button buttonRemove;
 
     @FXML
     private void closeApp(ActionEvent actionEvent) {
@@ -20,5 +38,24 @@ public class Controller {
         stage.setIconified(true);
     }
 
+    public void changeColorToYellow(ActionEvent actionEvent) {
+        colorChange("#ffbe2f", "#000000");
+    }
 
+    private void colorChange(String bgColor, String textColor) {
+        topPane.setStyle("-fx-background-color:" + bgColor);
+        buttonX.setStyle("-fx-background-color: " + bgColor + "; -fx-text-fill: " + textColor);
+        buttonMinimize.setStyle("-fx-background-color: " + bgColor + "; -fx-text-fill: " + textColor);
+        titleLabel.setStyle("-fx-text-fill: " + textColor);
+        backgroundPane.setStyle("-fx-border-color: " + bgColor);
+        buttonRemove.setStyle("-fx-background-color: " + bgColor + "; -fx-text-fill: " + textColor);
+    }
+
+    public void changeColorToRed(ActionEvent actionEvent) {
+        colorChange("#FF0000", "#FFFFFF");
+    }
+
+    public void changeColorToGreen(ActionEvent actionEvent) {
+        colorChange("#28cc42", "#000000");
+    }
 }
